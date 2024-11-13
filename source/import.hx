bd44c6d59a42cfaa30a30c8c67a9b7695cc7ad3c
Prohack101010 <miracberaber@gmail.com>
2024-11-13 15:34:03 +0300

    flixel

--- source/import.hx
+++ source/import.hx
@@ -9,3 +9,21 @@ import mobile.backend.Data;
 import mobile.flixel.FlxHitbox;
 import mobile.flixel.FlxVirtualPad;
 import mobile.flixel.FlxNewHitbox;
+
+//Flixel
+import flixel.FlxG;
+import flixel.FlxSprite;
+import flixel.FlxCamera;
+import flixel.util.FlxColor;
+import flixel.text.FlxText;
+import flixel.util.FlxTimer;
+import flixel.math.FlxMath;
+import flixel.math.FlxPoint;
+import flixel.tweens.FlxEase;
+import flixel.sound.FlxSound;
+import flixel.util.FlxDestroyUtil;
+import flixel.tweens.FlxTween;
+import flixel.group.FlxSpriteGroup;
+import flixel.group.FlxGroup.FlxTypedGroup;
+
+using StringTools;
