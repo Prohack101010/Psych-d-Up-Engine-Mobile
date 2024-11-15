05807f271b2d60242e45fb901f87975740b6279a
Prohack101010 <miracberaber@gmail.com>
2024-11-15 03:45:00 +0300

    Done ig

--- source/mobile/backend/MobileScaleMode.hx
+++ source/mobile/backend/MobileScaleMode.hx
@@ -1,6 +1,5 @@
 package mobile.backend;
 
-import flixel.FlxG;
 import flixel.system.scaleModes.BaseScaleMode;
 
 /**
@@ -9,47 +8,47 @@ import flixel.system.scaleModes.BaseScaleMode;
  */
 class MobileScaleMode extends BaseScaleMode
 {
-    public static var allowWideScreen(default, set):Bool = false;
+	public static var allowWideScreen(default, set):Bool = false;
 
-    override function updateGameSize(Width:Int, Height:Int):Void
+	override function updateGameSize(Width:Int, Height:Int):Void
 	{
-        if(allowWideScreen)
-        {
-            super.updateGameSize(Width, Height);
-        }
-        else
-        {
-            var ratio:Float = FlxG.width / FlxG.height;
-            var realRatio:Float = Width / Height;
-    
-            var scaleY:Bool = realRatio < ratio;
-    
-            if (scaleY)
-            {
-                gameSize.x = Width;
-                gameSize.y = Math.floor(gameSize.x / ratio);
-            }
-            else
-            {
-                gameSize.y = Height;
-                gameSize.x = Math.floor(gameSize.y * ratio);
-            }
-        }
+		if (allowWideScreen)
+		{
+			super.updateGameSize(Width, Height);
+		}
+		else
+		{
+			var ratio:Float = FlxG.width / FlxG.height;
+			var realRatio:Float = Width / Height;
+
+			var scaleY:Bool = realRatio < ratio;
+
+			if (scaleY)
+			{
+				gameSize.x = Width;
+				gameSize.y = Math.floor(gameSize.x / ratio);
+			}
+			else
+			{
+				gameSize.y = Height;
+				gameSize.x = Math.floor(gameSize.y * ratio);
+			}
+		}
 	}
 
-    override function updateGamePosition():Void
+	override function updateGamePosition():Void
 	{
-        if(allowWideScreen)
-		    FlxG.game.x = FlxG.game.y = 0;
-        else
-            super.updateGamePosition();
+		if (allowWideScreen)
+			FlxG.game.x = FlxG.game.y = 0;
+		else
+			super.updateGamePosition();
 	}
 
-    @:noCompletion
-    private static function set_allowWideScreen(value:Bool):Bool
-    {
-        allowWideScreen = value;
-        FlxG.scaleMode = new MobileScaleMode();
-        return value;
-    }
+	@:noCompletion
+	private static function set_allowWideScreen(value:Bool):Bool
+	{
+		allowWideScreen = value;
+		FlxG.scaleMode = new MobileScaleMode();
+		return value;
+	}
 }
