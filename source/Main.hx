b0241b45e68b00b9d56cf1b1cd4c437063df6708
Prohack101010 <miracberaber@gmail.com>
2024-11-15 16:33:46 +0300

    fax

--- source/Main.hx
+++ source/Main.hx
@@ -21,6 +21,9 @@ import mobile.states.CopyState;
 #if linux
 import lime.graphics.Image;
 
+import sys.FileSystem;
+import sys.io.File;
+
 @:cppInclude('./external/gamemode_client.h')
 @:cppFileCode('
 	#define GAMEMODE_AUTO
@@ -123,7 +126,6 @@ class Main extends Sprite
 		#end
 
 		ClientPrefs.loadDefaultKeys();
-		#if ACHIEVEMENTS_ALLOWED Achievements.load(); #end
 		addChild(new FlxGame(game.width, game.height, #if (mobile && MODS_ALLOWED) CopyState.checkExistingFiles() ? game.initialState : CopyState #else game.initialState #end, #if (flixel < "5.0.0") game.zoom, #end game.framerate, game.framerate, game.skipSplash, game.startFullscreen));
 
 		fpsVar = new FPSCounter(10, 3, 0xFFFFFF);
@@ -157,21 +159,6 @@ class Main extends Sprite
 		#if mobile
 		FlxG.scaleMode = new MobileScaleMode();
 		#end
-
-		// shader coords fix
-		FlxG.signals.gameResized.add(function (w, h) {
-			if(fpsVar != null)
-				fpsVar.positionFPS(10, 3, Math.min(w / FlxG.width, h / FlxG.height));
-		     if (FlxG.cameras != null) {
-			   for (cam in FlxG.cameras.list) {
-				if (cam != null && cam.filters != null)
-					resetSpriteCache(cam.flashSprite);
-			   }
-			}
-
-			if (FlxG.game != null)
-			resetSpriteCache(FlxG.game);
-		});
 	}
 
 	static function resetSpriteCache(sprite:Sprite):Void {
